e28d544761e81479a081c82d2b23476cdafed66c
Hogan Steffes <37481947+Hogan-Dazs@users.noreply.github.com>
2020-10-21 21:27:20 -0500

    Change to IE API for MetalPressRecipes

--- src/main/java/Hogan/uniTweak/integration/crafttweaker/crossmod/ImmersiveEngineering.java
+++ src/main/java/Hogan/uniTweak/integration/crafttweaker/crossmod/ImmersiveEngineering.java
@@ -6,12 +6,12 @@ import crafttweaker.annotations.ModOnly;
 import crafttweaker.annotations.ZenRegister;
 import crafttweaker.api.item.IItemStack;
 import crafttweaker.api.minecraft.CraftTweakerMC;
+import net.minecraft.item.ItemStack;
 import stanhebben.zenscript.annotations.Optional;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 import java.util.List;
 import blusunrize.immersiveengineering.api.crafting.MetalPressRecipe;
-import blusunrize.immersiveengineering.common.util.compat.crafttweaker.MetalPress;
 import wanion.unidict.UniDict;
 import wanion.unidict.api.*;
 import wanion.unidict.resource.Resource;
@@ -21,8 +21,6 @@ import wanion.unidict.resource.Resource;
 @ZenRegister
 public class ImmersiveEngineering {
 	
-	static UniDictAPI uniDictAPI = UniDict.getAPI();
-	
 	@ZenMethod
 	public static void pressRecipe(String outputKind, @Optional("1") int outCount, String inputKind, IItemStack mold, int energy, @Optional int inputSize) {
 		CraftTweakerAPI.apply(new pressRecipe(outputKind, outCount, inputKind, mold, energy, inputSize));
@@ -31,24 +29,26 @@ public class ImmersiveEngineering {
 	public static class pressRecipe implements IAction{
 		
 		int input, output, inNum, outNum, energy;
-		IItemStack mold;
+		ItemStack mold;
 
 		public pressRecipe(String outputKind, int outCount, String inputKind, IItemStack mold, int energy, int inputSize) {
 			input = Resource.getKindOfName(inputKind);
 			output = Resource.getKindOfName(outputKind);
-			this.mold=mold;
+			this.mold=CraftTweakerMC.getItemStack(mold);
 			inNum=inputSize;
 			outNum=outCount;
 		}
 		
 		@Override
 		public void apply() {
+			UniDictAPI uniDictAPI = UniDict.getAPI();
 			List<Resource> inAndOut = uniDictAPI.getResources(input, output);
 			
 			for(Resource resource : inAndOut) {
-				IItemStack outStack = CraftTweakerMC.getIItemStack(resource.getChild(output).getMainEntry(outNum));
-				IItemStack inStack = CraftTweakerMC.getIItemStack(resource.getChild(input).getMainEntry());
-				MetalPress.addRecipe(outStack, inStack, mold, energy, inNum);
+				ItemStack outStack = resource.getChild(output).getMainEntry(outNum);
+				ItemStack inStack = resource.getChild(input).getMainEntry(inNum);
+				//MetalPress.addRecipe(outStack, inStack, mold, energy, inNum);
+				MetalPressRecipe.addRecipe(outStack, inStack, mold, energy);
 			}
 		}
 
