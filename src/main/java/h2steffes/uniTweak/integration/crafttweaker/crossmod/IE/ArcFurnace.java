e6f68a2c1aeffb8a0bd47dec290a78e4bfcd4879
Hannah <hannahks@protonmail.com>
2021-12-12 13:09:41 -0800

    Arc Furnace add method

--- src/main/java/h2steffes/uniTweak/integration/crafttweaker/crossmod/IE/ArcFurnace.java
+++ src/main/java/h2steffes/uniTweak/integration/crafttweaker/crossmod/IE/ArcFurnace.java
@@ -2,13 +2,16 @@ package h2steffes.uniTweak.integration.crafttweaker.crossmod.IE;
 
 import java.util.List;
 
-import blusunrize.immersiveengineering.api.crafting.CrusherRecipe;
+import blusunrize.immersiveengineering.api.crafting.ArcFurnaceRecipe;
+import blusunrize.immersiveengineering.common.util.compat.crafttweaker.CraftTweakerHelper;
 import crafttweaker.IAction;
 import crafttweaker.annotations.ModOnly;
 import crafttweaker.annotations.ZenRegister;
 import crafttweaker.api.item.IIngredient;
 import crafttweaker.api.item.IItemStack;
+import crafttweaker.api.oredict.IOreDictEntry;
 import crafttweaker.mc1120.CraftTweaker;
+import h2steffes.uniTweak.util.ResourceHandling;
 import net.minecraft.item.ItemStack;
 import stanhebben.zenscript.annotations.Optional;
 import stanhebben.zenscript.annotations.ZenClass;
@@ -23,8 +26,8 @@ import wanion.unidict.resource.Resource;
 public class ArcFurnace {
 
 	@ZenMethod
-	public static void add(String outputKind, String inputKind, IItemStack slag, int time, int energyPerTick, @Optional IIngredient[] additives, @Optional String specialRecipeType) {
-		CraftTweaker.LATE_ACTIONS.add(new Add());
+	public static void add(String outputKind, String inputKind, IItemStack slag, int time, int energyPerTick, @Optional(valueLong = 1) int outCount, @Optional(valueLong = 1) int inputSize, @Optional IIngredient[] additives, @Optional String specialRecipeType) {
+		CraftTweaker.LATE_ACTIONS.add(new Add(outputKind, inputKind, slag, time, energyPerTick, outCount, inputSize, additives, specialRecipeType));
 	}
 	
 	@ZenMethod
@@ -33,12 +36,45 @@ public class ArcFurnace {
 	}
 
 	public static class Add implements IAction {
+		
+		String inputKindString, outputKindString, specialRecipeType;
+		int energyPerTick, time, inputCount, outputCount;
+		IItemStack slag;
+		Object[] adds = null;
 
-		public Add() {
+		public Add(String outputKind, String inputKind, IItemStack slag, int time, int energyPerTick, int outputCount, int inputCount, IIngredient[] additives, String specialRecipeType) {
+			outputKindString = outputKind;
+			inputKindString = inputKind;
+			this.slag = slag;
+			this.time = time;
+			this.energyPerTick = energyPerTick;
+			this.specialRecipeType = specialRecipeType;
+			this.inputCount = inputCount;
+			this.outputCount = outputCount;
+			
+			if(additives!=null)
+			{
+				adds = new Object[additives.length];
+				for(int i = 0; i < additives.length; i++)
+					adds[i] = CraftTweakerHelper.toObject(additives[i]);
+			}
 		}
 
 		@Override
 		public void apply() {
+			final UniDictAPI uniDictAPI = UniDict.getAPI();
+			int outputKindInt = Resource.getKindFromName(outputKindString);
+			int inputKindInt = Resource.getKindFromName(inputKindString);
+			List<Resource> matchingResources = uniDictAPI.getResources(outputKindInt, inputKindInt);
+
+			for (Resource resource : matchingResources) {
+				ItemStack outputStack = resource.getChild(outputKindInt).getMainEntry(outputCount);
+				IOreDictEntry inputOreDictEntry = ResourceHandling.getOreDictEntry(resource, inputKindInt);
+				ArcFurnaceRecipe recipe = new ArcFurnaceRecipe(outputStack, CraftTweakerHelper.toObject(inputOreDictEntry.amount(inputCount)), CraftTweakerHelper.toStack(slag), time, energyPerTick, adds);
+				if(specialRecipeType!=null)
+					recipe.setSpecialRecipeType(specialRecipeType);
+				ArcFurnaceRecipe.recipeList.add(recipe);
+			}
 		}
 
 		@Override
@@ -64,7 +100,7 @@ public class ArcFurnace {
 			for (Resource resource : matchingResources) {
 				List<ItemStack> outputList = resource.getChild(outputKindInt).getEntries();
 				for (ItemStack output : outputList) {
-					CrusherRecipe.removeRecipesForOutput(output);
+					ArcFurnaceRecipe.removeRecipes(output);
 				}
 			}
 		}
