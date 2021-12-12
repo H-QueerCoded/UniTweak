d6841f89e8e6b1661150f34e55725aeffb1d2885
Hannah <hannahks@protonmail.com>
2021-12-12 13:07:05 -0800

    Remove proxy

--- src/main/java/h2steffes/uniTweak/UniTweak.java
+++ src/main/java/h2steffes/uniTweak/UniTweak.java
@@ -1,11 +1,6 @@
 package h2steffes.uniTweak;
 
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.SidedProxy;
 
 @Mod(modid = UniTweak.MODID, name = UniTweak.NAME, useMetadata = true)
 public class UniTweak
@@ -15,25 +10,4 @@ public class UniTweak
     
     @Mod.Instance
     public static UniTweak instance;
-    
-    @SidedProxy(clientSide = "h2steffes.uniTweak.clientProxy", serverSide = "h2steffes.uniTweak.commonProxy")
-    public static commonProxy proxy;
-
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-    	proxy.preInit();
-    }
-
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-    	proxy.init();
-    }
-    
-    @EventHandler
-    public void postInit(FMLPostInitializationEvent event)
-    {
-        proxy.postInit();
-    }
 }
--- src/main/java/h2steffes/uniTweak/clientProxy.java
+++
@@ -1,5 +0,0 @@
-package h2steffes.uniTweak;
-
-public class clientProxy extends commonProxy {
-	
-}
